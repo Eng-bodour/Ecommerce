262b7c1b3f756de20e4244c58ef602e5f60a3c7a
bodour <git@github.com>
2022-01-06 21:02:22 +0300

    TextFormField and Form and Key Global

--- lib/screens/signupscreen.dart
+++ lib/screens/signupscreen.dart
@@ -10,79 +10,85 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _globalKey = GlobalKey<FormState>();
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: KMainColor,
-      body: ListView(
-        children: [
-          const LogoFile(),
-          SizedBox(
-            height: height * .1,
-          ),
-          CustomTextField(
-              hintText: 'Enter your Name',
-              iconData: const Icon(
-                Icons.perm_identity,
-                color: KMainColor,
-              )),
-          SizedBox(
-            height: height * .02,
-          ),
-          CustomTextField(
-            hintText: "Enter your Email",
-            iconData: const Icon(
-              Icons.email,
-              color: KMainColor,
+      body: Form(
+        key: _globalKey,
+        child: ListView(
+          children: [
+            const LogoFile(),
+            SizedBox(
+              height: height * .1,
             ),
-          ),
-          SizedBox(
-            height: height * .02,
-          ),
-          CustomTextField(
-              hintText: 'Enter your Password',
-              iconData: const Icon(
-                Icons.lock,
-                color: KMainColor,
-              )),
-          SizedBox(
-            height: height * .05,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 200),
-            child: FlatButton(
-                shape: RoundedRectangleBorder(
-                    //  side: const BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.black,
-                onPressed: () {},
-                child: const Text(
-                  'Signup',
-                  style: TextStyle(color: Colors.white),
+            CustomTextField(
+                hintText: 'Enter your Name',
+                iconData: const Icon(
+                  Icons.perm_identity,
+                  color: KMainColor,
                 )),
-          ),
-          SizedBox(
-            height: height * .05,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            // ignore: prefer_const_literals_to_create_immutables
-            children: [
-              const Text(
-                'Do have an account?',
-                style: TextStyle(color: Colors.white),
+            SizedBox(
+              height: height * .02,
+            ),
+            CustomTextField(
+              hintText: "Enter your Email",
+              iconData: const Icon(
+                Icons.email,
+                color: KMainColor,
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/login');
-                },
-                child: const Text(
-                  'login',
-                  style: TextStyle(color: Colors.black),
+            ),
+            SizedBox(
+              height: height * .02,
+            ),
+            CustomTextField(
+                hintText: 'Enter your Password',
+                iconData: const Icon(
+                  Icons.lock,
+                  color: KMainColor,
+                )),
+            SizedBox(
+              height: height * .05,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 200),
+              child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                      //  side: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Colors.black,
+                  onPressed: () {
+                    _globalKey.currentState?.validate();
+                  },
+                  child: const Text(
+                    'Signup',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+            SizedBox(
+              height: height * .05,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Text(
+                  'Do have an account?',
+                  style: TextStyle(color: Colors.white),
                 ),
-              ),
-            ],
-          )
-        ],
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: const Text(
+                    'login',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widget/custumtextfield.dart
+++ lib/widget/custumtextfield.dart
@@ -6,6 +6,18 @@ class CustomTextField extends StatelessWidget {
   final String hintText;
   final Icon iconData;
 
+  String _errorMassage(String er) {
+    switch (hintText) {
+      case 'Enter your Name':
+        return 'Name is Empty';
+      case 'Enter your Email':
+        return 'Email is Empty';
+      case 'Enter your Password':
+        return 'Password is Empty';
+    }
+    return '';
+  }
+
   // ignore: prefer_const_constructors_in_immutables
   CustomTextField({Key? key, required this.hintText, required this.iconData})
       : super(key: key);
@@ -15,10 +27,18 @@ class CustomTextField extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: TextFormField(
+        validator: (value) {
+          if (value!.isEmpty) {
+            return _errorMassage(hintText);
+          }
+        },
         cursorColor: KSeconderyColor,
         decoration: InputDecoration(
           hintText: hintText,
           prefixIcon: iconData,
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.white)),
           enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(20),
               borderSide: const BorderSide(color: Colors.white)),
